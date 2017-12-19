14c2f5e3bc7636afe4cde3756e115c3c28a70f54
Miki Ueno <ueno@imc.tut.ac.jp>
2017-12-19 17:17:41 +0900

    initial commit

---
+++ src/StudentCard.java
@@ -0,0 +1,15 @@
+
+public class StudentCard {
+	private int accountBalance = 0;
+
+	public int getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(int accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+	
+
+}
